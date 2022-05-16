1304e0c501e2c6361df13c7fabe368e902f54caa
josh@jmac <joshashep@gmail.com>
2022-05-16 17:03:54 -0500

    try to use a typescript file

---
+++ renderer.js
@@ -0,0 +1,6 @@
+var mapState = {
+    'entities': [],
+    'items': []
+};
+var el = document.getElementById('tablee').getElementsByTagName('tbody')[0];
+el.insertRow();
---
+++ renderer.ts
@@ -0,0 +1,7 @@
+const mapState = {
+    'entities': [],
+    'items': [],
+};
+
+const el = document.getElementById('tablee').getElementsByTagName('tbody')[0];
+el.insertRow();
