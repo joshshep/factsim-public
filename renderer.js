5488fb2d4597dd0cd2c31b8b6eef93d61c3ee8d1
josh@jmac <joshashep@gmail.com>
2022-05-16 18:13:58 -0500

    svg is probably easiest

--- renderer.js
+++ renderer.js
@@ -1,7 +1,56 @@
 "use strict";
-const mapState = {
-    'entities': [],
-    'items': [],
+const mapStates = {
+    "states": [
+        {
+            "tick": 0,
+            "entities_add": [
+                {
+                    "loc": [
+                        0,
+                        0
+                    ],
+                    "sprite-url": "sprites/straight_belt.png",
+                    "rotate": 1
+                },
+                {
+                    "loc": [
+                        0,
+                        1
+                    ],
+                    "sprite-url": "sprites/straight_belt.png",
+                    "rotate": 1
+                }
+            ],
+            "items_update": [
+                {
+                    "id": 1000,
+                    "sprite-url": "sprites/items/iron_plate.png",
+                    "loc": [
+                        0.25,
+                        0.25
+                    ]
+                }
+            ]
+        },
+        {
+            "tick": 1,
+            "entities_rm": [
+                [
+                    0,
+                    1
+                ]
+            ],
+            "items_update": [
+                {
+                    "id": 1000,
+                    "loc": [
+                        0.5,
+                        0.25
+                    ]
+                }
+            ]
+        }
+    ]
 };
 // This feels too simple
 function castExists(thing) {
@@ -10,3 +59,5 @@ function castExists(thing) {
 const el = castExists(document.getElementById('tablee')).getElementsByTagName('tbody')[0];
 el.insertRow();
 el.insertRow();
+const svgg = castExists(document.getElementById('svgg'));
+svgg.innerHTML += '<image x="0" y="0" width="275" height="95" xlink:href="sprites/straight_belt.png" />';
